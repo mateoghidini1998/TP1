e10fb8257a4f9230720b1e3e7f2c3e61fd6c8db8
JuanIgnacioBellavitis <juan.bellavitis@gmail.com>
2021-03-25 18:32:25 -0300

    cambios

--- TrabajoPractico1/TrabajoPractico1/Form1.Designer.cs
+++ TrabajoPractico1/TrabajoPractico1/Form1.Designer.cs
@@ -74,10 +74,12 @@ namespace TrabajoPractico1
             // 
             this.textBox1.AcceptsTab = true;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(149, 163);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1.Location = new System.Drawing.Point(139, 163);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
             this.textBox1.Size = new System.Drawing.Size(339, 162);
             this.textBox1.TabIndex = 3;
             this.textBox1.Text = resources.GetString("textBox1.Text");
