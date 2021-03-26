834112798f281946b95f2992140ba53c8d7c5ee1
Julian R <julianriscalatrullas97@hotmail.com>
2021-03-26 14:14:54 -0300

    Prueba?

--- TrabajoPractico1/TrabajoPractico1/Form3.Designer.cs
+++ TrabajoPractico1/TrabajoPractico1/Form3.Designer.cs
@@ -30,6 +30,7 @@ namespace TrabajoPractico1
         private void InitializeComponent()
         {
             this.btnVolverEj2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnVolverEj2
@@ -43,21 +44,33 @@ namespace TrabajoPractico1
             this.btnVolverEj2.UseVisualStyleBackColor = true;
             this.btnVolverEj2.Click += new System.EventHandler(this.btnVolverEj2_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(299, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "prueba";
+            // 
             // frmEjercicio2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnVolverEj2);
             this.Name = "frmEjercicio2";
             this.Text = "Ejercicio 2";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnVolverEj2;
+        private System.Windows.Forms.Label label1;
     }
 }
